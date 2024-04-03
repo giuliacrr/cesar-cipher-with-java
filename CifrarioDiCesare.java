74e39c9d9735832fbeb0bda439c834550ad23767
Giulia Corrado <giulia.corrado02@gmail.com>
2024-04-03 11:58:27 +0200

    Added comments

--- CifrarioDiCesare.java
+++ CifrarioDiCesare.java
@@ -9,20 +9,20 @@ public class CifrarioDiCesare {
         // When user passes <messaggio> <numero_di_shift> <modalità(1/0)>
         // where 0 -> Crittografia , 1 -> Decrittografia)>
 
-        String messaggio = args[0].toLowerCase(); // convertiamo tutti i caratteri in lower case per semplicità
-        int shift = Integer.parseInt(args[1]);
-        int modalità = Integer.parseInt(args[2]);
-        String risultato = "";
+        String messaggio = args[0].toLowerCase(); // to simplify, let's lowercase the string
+        int shift = Integer.parseInt(args[1]); // Number of shift (or "jumps") that we do to change index
+        int modalità = Integer.parseInt(args[2]); // where 0 -> Crittografia , 1 -> Decrittografia
+        String risultato = ""; // String where we will push result once encrypted/decrypted
 
         if (modalità == 1) {
-            risultato = cripta(messaggio, shift);
+            risultato = cripta(messaggio, shift); // call the method where args will be text and number of shifts
         } else if (modalità == 0) {
             risultato = decripta(messaggio, shift);
-        } else {
+        } else { // Else will trigger if user enters something != 1 or 0
             System.out.println("Modalità non valida. Usa 1 per Crittografia e 0 per decrittografia");
             return;
         }
-        System.out.println("Risultato : " + risultato);
+        System.out.println("Risultato : " + risultato); // Print on console the result
     }
 
     private static String cripta(String testo, int shift) {
@@ -30,24 +30,26 @@ public class CifrarioDiCesare {
     }
 
     private static String decripta(String testo, int shift) {
-        return trasforma(testo, -shift);
+        return trasforma(testo, -shift); // shift is negative, since we have to go backwards while decrypting
     }
 
     public static String trasforma(String testo, int shift) {
-        StringBuilder risultato = new StringBuilder();
+        StringBuilder risultato = new StringBuilder(); // Will make result in a string from array
 
-        for (char carattere : testo.toCharArray()) {
+        for (char carattere : testo.toCharArray()) { // We iterate each character of the text that we transform in an
+                                                     // Array of chars
             if (DIZIONARIO.indexOf(carattere) != -1) {
                 int posizioneOriginale = DIZIONARIO.indexOf(carattere);
-                // Effetto supermario - l'indice non va outofbound usando % diz.length()
+                // Supermario effect - like this, index doesn't trigger OutOfBoundException and
+                // % will give us back the new position of the character
                 int nuovaPosizione = (DIZIONARIO.length() + posizioneOriginale + shift) % DIZIONARIO.length();
-                // int nuovaPosizione = posizioneOriginale + shift;
-                risultato.append(DIZIONARIO.charAt(nuovaPosizione));
+                risultato.append(DIZIONARIO.charAt(nuovaPosizione));// Adds the character that corresponds to the new
+                                                                    // position
             } else {
-                risultato.append(carattere);
+                risultato.append(carattere); // Adds them into Risultato string
             }
         }
-        return risultato.toString();
+        return risultato.toString(); // Returns the result risultato
     }
 
 }
